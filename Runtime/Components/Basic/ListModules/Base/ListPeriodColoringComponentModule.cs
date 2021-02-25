eab7ca23eeae3ddeff128f38fba7cdc36413d9e0
Alex Mironov <zettaiingamono@gmail.com>
2021-02-25 14:01:50 +0300

    ListComponent color rows module

---
+++ Runtime/Components/Basic/ListModules/Base/ListPeriodColoringComponentModule.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine.UI.Windows.Components;
+
+namespace UnityEngine.UI.Windows {
+
+	[ComponentModuleDisplayName("Periodic rows coloring")]
+	public class ListPeriodColoringComponentModule : ListComponentModule {
+
+		public ImageComponent background;
+		public int period;
+		public Color firstColor;
+		public Color secondColor;
+
+		public override void OnShowBegin() {
+
+			base.OnShowBegin();
+
+			if(period == 0) return;
+
+			var counter = 1;
+
+			for (int i = 0; i < listComponent.items.Count; i++) {
+
+				var imageObject = listComponent.items[i].transform.Find(this.background.name);
+
+				if (counter <= this.period) {
+
+					imageObject.GetComponent<ImageComponent>().SetColor(this.firstColor);
+
+				}
+				else {
+
+					imageObject.GetComponent<ImageComponent>().SetColor(this.secondColor);
+
+				}
+
+				counter++;
+
+				if (counter > this.period * 2) {
+
+					counter = 1;
+
+				}
+
+			}
+
+		}
+
+	}
+
+}

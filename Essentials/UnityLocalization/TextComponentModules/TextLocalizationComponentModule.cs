538aed1177f05da8537b9ba9de4ec4d57e62475d
Alex Silaev <chrome.alex@gmail.com>
2021-05-05 10:42:51 +0300

    Unity Localization support added

---
+++ Essentials/UnityLocalization/TextComponentModules/TextLocalizationComponentModule.cs
@@ -0,0 +1,40 @@
+﻿
+namespace UnityEngine.UI.Windows {
+
+    public class TextLocalizationComponentModule : TextComponentModule {
+
+        public UnityEngine.Localization.LocalizedString key;
+
+        public override void OnInit() {
+            
+            base.OnInit();
+
+            this.key.StringChanged += this.OnChanged;
+
+        }
+
+        public override void OnDeInit() {
+            
+            this.key.StringChanged -= this.OnChanged;
+            
+            base.OnDeInit();
+            
+        }
+
+        public override void OnShowBegin() {
+            
+            base.OnShowBegin();
+
+            this.key.RefreshString();
+
+        }
+
+        private void OnChanged(string val) {
+            
+            this.textComponent.SetText(val);
+            
+        }
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# ME.UI.Windows submodule
+
+This project has no documentation yet, but it is similar to it's first version:
+https://github.com/chromealex/Unity3d.UI.Windows

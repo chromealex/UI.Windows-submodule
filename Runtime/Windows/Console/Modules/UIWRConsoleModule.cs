7fe571a250e083f7aecea73d3f1ebbebc32e327b
Alex Silaev <chrome.alex@gmail.com>
2021-09-17 10:35:42 +0300

    Console: UIWR Static command added

--- Runtime/Windows/Console/Modules/UIWRConsoleModule.cs
+++ Runtime/Windows/Console/Modules/UIWRConsoleModule.cs
@@ -32,7 +32,7 @@ namespace UnityEngine.UI.Windows.Runtime.Windows {
         }
 
         [Help("Prints all assets in bundle")]
-        public void Get(string bundleName) {
+        public void Bundle(string bundleName) {
 
             var allBundles = AssetBundle.GetAllLoadedAssetBundles();
             foreach (var bundle in allBundles) {
@@ -48,6 +48,54 @@ namespace UnityEngine.UI.Windows.Runtime.Windows {
                     
                 }
 
+            }
+
+        }
+
+        [Help("Prints all static non-null references")]
+        public void Static() {
+
+            Debug.Log("Static Fields:");
+            var asms = System.AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var asm in asms) {
+
+                if (asm.FullName.StartsWith("mscorlib") == true) continue;
+                if (asm.FullName.StartsWith("Unity") == true) continue;
+                if (asm.FullName.StartsWith("System") == true) continue;
+                if (asm.FullName.StartsWith("Mono") == true) continue;
+                if (asm.FullName.StartsWith("Assembly-CSharp-Editor") == true) continue;
+                
+                var types = asm.GetTypes();
+                foreach (var type in types) {
+
+                    var fields = type.GetFields(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Public);
+                    foreach (var field in fields) {
+
+                        try {
+
+                            if (field.FieldType.IsEnum == true) continue;
+                            if (field.FieldType.IsPointer == true) continue;
+                            if (field.FieldType.IsPrimitive == true) continue;
+                            if (field.FieldType == typeof(string)) continue;
+                            
+                            var val = field.GetValue(null);
+                            if (val != null) {
+
+                                var prev = UnityEngine.Application.GetStackTraceLogType(LogType.Log);
+                                UnityEngine.Application.SetStackTraceLogType(UnityEngine.LogType.Log, StackTraceLogType.None);
+                                Debug.Log(asm.FullName);
+                                Debug.Log(type.FullName);
+                                Debug.Log("  " + field.Name + ": " + val);
+                                UnityEngine.Application.SetStackTraceLogType(UnityEngine.LogType.Log, prev);
+
+                            }
+                            
+                        } catch (System.Exception) { }
+
+                    }
+
+                }
+                
             }
 
         }

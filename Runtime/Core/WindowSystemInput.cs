dd24019e9e58924fc6fb80621e4e3904510b9253
HDHero <leodenx@gmail.com>
2025-02-18 14:56:10 +0100

    Added WindowSystemInput

---
+++ Runtime/Core/WindowSystemInput.cs
@@ -0,0 +1,11 @@
+﻿namespace UnityEngine.UI.Windows {
+
+    public class WindowSystemInput {
+
+        private class HasPointerClickThisFrameFieldKey {}
+        
+        public static readonly Unity.Burst.SharedStatic<bool> hasPointerClickThisFrame = 
+            Unity.Burst.SharedStatic<bool>.GetOrCreate<WindowSystemInput, HasPointerClickThisFrameFieldKey>();
+    }
+
+}

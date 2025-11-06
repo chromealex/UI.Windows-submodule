d05cc6fa2d49dc83141a4ca6697a51f2048a6516
Alex Silaev <chrome.alex@gmail.com>
2025-11-06 15:55:23 +0300

    [Tutorial] Reset interactables added

---
+++ Essentials/Tutorial/Runtime/Types/Actions/ResetInteractablesState.cs
@@ -0,0 +1,18 @@
+﻿namespace UnityEngine.UI.Windows.Essentials.Tutorial {
+
+    [ComponentModuleDisplayName("UI/Reset Interactables state")]
+    public struct ResetInteractablesState : IAction {
+
+        public string text => $"Unlock all interactables";
+
+        public ActionResult Execute(in Context context) {
+
+            WindowSystem.CancelWaitInteractables();
+                
+            return ActionResult.MoveNext;
+
+        }
+
+    }
+
+}

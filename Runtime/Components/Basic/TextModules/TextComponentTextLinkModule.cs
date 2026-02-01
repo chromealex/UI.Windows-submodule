ad12e41550da9283fa85ed0ab5341537b0d6c4e9
Alex Silaev <chrome.alex@gmail.com>
2026-02-01 23:31:15 +0300

    [Components] TextComponent: link recongnize module added for TMP

---
+++ Runtime/Components/Basic/TextModules/TextComponentTextLinkModule.cs
@@ -0,0 +1,23 @@
+﻿namespace UnityEngine.UI.Windows {
+
+    public class TextComponentTextLinkModule : TextComponentModule, UnityEngine.EventSystems.IPointerClickHandler {
+
+        public string id;
+        public string link;
+        
+        public void OnPointerClick(UnityEngine.EventSystems.PointerEventData eventData) {
+            #if TEXTMESHPRO_SUPPORT
+            if (this.textComponent.graphics is TMPro.TMP_Text text) {
+                var index = TMPro.TMP_TextUtilities.FindIntersectingCharacter(text, WindowSystem.GetPointerPosition(), this.GetWindow().workCamera, true);
+                if (index < 0) return;
+                var link = text.textInfo.linkInfo[index];
+                if (link.GetLinkID() == this.id) {
+                    Application.OpenURL(this.link);
+                }
+            }
+            #endif
+        }
+        
+    }
+    
+}
